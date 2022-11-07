f636b80a9ce413a9472a578bb1c2c906b2e886f3
Pedro Julian Palavecino <ppalavecino@teledoc.cl>
2022-11-06 22:16:41 -0300

    Added main and secondary fire damage

--- Juego/Enemigo/EnemigoDummy.gd
+++ Juego/Enemigo/EnemigoDummy.gd
@@ -2,6 +2,15 @@ class_name EnemigoDummy
 extends Node2D
 
 
+var hitpoints:float = 10.0
+
 func _on_Area2D_body_entered(body: Node) -> void:
 	if body is Player:
 		body.destruir()
+
+
+func recibir_danio(danio: float) -> void:
+	hitpoints -= danio
+	print(danio)
+	if hitpoints <= 0.0:
+		queue_free()
