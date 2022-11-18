2af6f9baca148ab513bbae89deafb1fa35b38237
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2022-11-18 19:40:30 -0300

    Add meteorite explosion sfx

--- Juego/Explosiones/ExplosionMeteorito/ExplosionMeteorito.gd
+++ Juego/Explosiones/ExplosionMeteorito/ExplosionMeteorito.gd
@@ -1,7 +1,10 @@
 class_name ExplosionMeteorito
 extends Node2D
 
+onready var destroy_sfx:AudioStreamPlayer2D = $DestroySFX
+
 func _ready():
+	destroy_sfx.play()
 	$AnimationPlayer.play("explosion")
 
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
