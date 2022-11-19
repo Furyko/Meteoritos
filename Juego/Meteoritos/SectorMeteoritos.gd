addf7a2519cc393f4ee2399b85edd92d5d2798ae
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2022-11-18 22:17:40 -0300

    Update meteorites
    Now the meteorites spawn randomly

--- Juego/Meteoritos/SectorMeteoritos.gd
+++ Juego/Meteoritos/SectorMeteoritos.gd
@@ -1,8 +1,30 @@
-class_name SectrMeteoritos
+class_name SectorMeteoritos
 extends Node2D
 
+## Export Attributes
+export var cantidad_meteoritos:int = 10
+
+## Attributes
+var spawners:Array
+
+## Methods
+func _ready() -> void:
+	almacenar_spawners()
+
+## Custom Methods
+func almacenar_spawners() -> void:
+	for spawner in $Spawners.get_children():
+		spawners.append(spawner)
+		
+func spawner_aleatorio() -> int:
+	randomize()
+	return randi() % spawners.size()
 
 ## Intern Signals
 func _on_SpawnTimer_timeout():
-	for spawner in $Spawners.get_children():
-		spawner.spawnear_meteorito()
+	if cantidad_meteoritos == 0:
+		$SpawnTimer.stop()
+		return
+	spawners[spawner_aleatorio()].spawnear_meteorito()
+	cantidad_meteoritos -= 1
+	
