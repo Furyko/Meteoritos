f9b9aa379e6524365ef9a7477be0614e89f1ae31
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2023-10-26 19:08:17 -0300

    Zoom improve
    Zoom transition improve
    
    Refactor camera

--- Juego/Niveles/Nivel.gd
+++ Juego/Niveles/Nivel.gd
@@ -47,7 +47,8 @@ func crear_sector_meteoritos(centro_camara:Vector2, numero_peligros:int) -> void
 	new_sector_meteoritos.crear(centro_camara, numero_peligros)
 	camara_nivel.global_position = centro_camara
 	contenedor_sector_meteoritos.add_child(new_sector_meteoritos)
-	$Player/CamaraPlayer.devolver_zoom_original()
+	camara_nivel.zoom = $Player/CamaraPlayer.zoom
+	camara_nivel.devolver_zoom_original()
 	transicion_camaras(
 		$Player/CamaraPlayer.global_position,
 		camara_nivel.global_position,
@@ -74,6 +75,9 @@ func controlar_meteoritos_restantes() -> void:
 	if meteoritos_totales == 0:
 		contenedor_sector_meteoritos.get_child(0).queue_free()
 		$Player/CamaraPlayer.set_puede_hacer_zoom(true)
+		var zoom_actual = $Player/CamaraPlayer.zoom
+		$Player/CamaraPlayer.zoom = camara_nivel.zoom
+		$Player/CamaraPlayer.zoom_suavizado(zoom_actual.x, zoom_actual.y, 1.0)
 		transicion_camaras(
 			camara_nivel.global_position,
 			$Player/CamaraPlayer.global_position,
