13ffd51994e84ca0ca1db34e9c0a62867be29e47
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2023-10-29 17:33:37 -0300

    Improve nave explosion
    Fix bugs
    
    Improve cameras behaviour

--- Juego/Explosiones/ExplosionMeteorito/ExplosionMeteorito.gd
+++ Juego/Explosiones/ExplosionMeteorito/ExplosionMeteorito.gd
@@ -4,8 +4,16 @@ extends Node2D
 onready var destroy_sfx:AudioStreamPlayer2D = $DestroySFX
 
 func _ready():
-	destroy_sfx.play()
+	# destroy_sfx.play()
 	$AnimationPlayer.play("explosion")
+
+func elegir_explosion_aleatoria() -> String:
+	randomize()
+	var num_anim:int = $AnimationPlayer.get_animation_list().size() - 1
+	var indice_anim_aleatoria:int = randi() % num_anim + 1
+	var lista_animacion:Array = $AnimationPlayer.get_animation_list()
+	
+	return lista_animacion[indice_anim_aleatoria]
 
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
 	queue_free()
