a231c8efeff46d9a9759af81a18c4761c6c1b42e
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2023-11-26 16:54:58 -0300

    Add energy bar to recharge stations

--- Juego/EstacionRecarga.gd
+++ Juego/EstacionRecarga.gd
@@ -11,8 +11,13 @@ var player_en_zona:bool = false
 
 ## Onready Attributes
 onready var carga_sfx:AudioStreamPlayer = $CargaSFX
+onready var barra_energia:ProgressBar = $BarraEnergia
 
 ## Methods
+func _ready() -> void:
+	barra_energia.max_value = energia
+	barra_energia.value = energia
+
 func _unhandled_input(event: InputEvent) -> void:
 	if not puede_recargar(event):
 		return
@@ -43,6 +48,8 @@ func controlar_energia() -> void:
 	energia -= radio_energia_entregada
 	if energia <= 0.0:
 		$VacioSFX.play()
+		
+	barra_energia.value = energia
 	
 ## Internal Signals
 func _on_AreaColision_body_entered(body:Node) -> void:
