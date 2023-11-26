4e482a72bddb7bc3e5561f97450f5d580fca32a7
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2023-11-26 14:39:38 -0300

    Add hud hidden when player is destroyed

--- Juego/UI/HUD/ContenedorInformacion.gd
+++ Juego/UI/HUD/ContenedorInformacion.gd
@@ -9,26 +9,39 @@ onready var animaciones:AnimationPlayer = $AnimationPlayer
 ## Export Attributes
 export var auto_ocultar:bool = false setget set_auto_ocultar
 
+## Attributes
+var esta_activo:bool = true setget set_esta_activo
+
 ## Methods
 func mostrar() -> void:
-	animaciones.play("mostrar")
+	if esta_activo:
+		animaciones.play("mostrar")
 	
 func ocultar() -> void:
+	if not esta_activo:
+		animaciones.stop()
 	animaciones.play("ocultar")
 	
-func set_auto_ocultar(ocultar: bool) -> void:
-	auto_ocultar = ocultar
-	
 func mostrar_suavizado() -> void:
+	if not esta_activo:
+		return
 	animaciones.play("mostrar_suavizado")
 	if auto_ocultar:
 		auto_ocultar_timer.start()
 	
 func ocultar_suavizado() -> void:
-	animaciones.play("ocultar_suavizado")
+	if esta_activo:
+		animaciones.play("ocultar_suavizado")
 
 func modificar_texto(text: String) -> void:
 	texto_contenedor.text = text
 
 func _on_AutoOcultarTimer_timeout() -> void:
 	ocultar_suavizado()
+
+## Setter and Getters
+func set_esta_activo(valor: bool) -> void:
+	esta_activo = valor
+	
+func set_auto_ocultar(ocultar: bool) -> void:
+	auto_ocultar = ocultar
