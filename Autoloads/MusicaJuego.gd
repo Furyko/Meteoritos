215672b914891e068c37eab1b9a894320c1901b0
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2023-11-26 17:53:31 -0300

    Add main menu

--- Autoloads/MusicaJuego.gd
+++ Autoloads/MusicaJuego.gd
@@ -12,6 +12,9 @@ onready var musica_nivel:AudioStreamPlayer = $MusicaNivel
 onready var musica_combate:AudioStreamPlayer = $MusicaCombate
 onready var tween_on:Tween = $TweenMusicaOn
 onready var tween_off:Tween = $TweenMusicaOff
+onready var lista_musicas:Dictionary = {
+	"menu_principal": $MusicaMenuPrincipal
+} setget ,get_lista_musicas
 
 ## Custom Methods
 func set_streams(stream_musica: AudioStream, stream_combate: AudioStream) -> void:
@@ -21,6 +24,13 @@ func set_streams(stream_musica: AudioStream, stream_combate: AudioStream) -> voi
 func play_musica_nivel() -> void:
 	stop_todo()
 	musica_nivel.play()
+	
+func play_musica(musica: AudioStreamPlayer) -> void:
+	stop_todo()
+	musica.play()
+	
+func play_boton() -> void:
+	$BotonMenu.play()
 
 func stop_todo() -> void:
 	for nodo in get_children():
@@ -62,6 +72,10 @@ func fade_out(musica_fade_out: AudioStreamPlayer) -> void:
 		Tween.EASE_IN_OUT
 	)
 	tween_off.start()
+
+## Setters and Getters
+func get_lista_musicas() -> Dictionary:
+	return lista_musicas
 
 func _on_TweenMusicaOff_tween_completed(object, key) -> void:
 	object.stop()
